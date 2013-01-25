daac8543d0ff3ab5747c752b534fc048ff74b95c
Adam Flores <rendenmedia@gmail.com>
2013-01-25 18:13:49 -0500

    Cocos2d Testing
    Part 1

--- LearnPython273Aptana/my-csv-import-hello-world-01.py
+++ LearnPython273Aptana/my-csv-import-hello-world-01.py
@@ -2,16 +2,6 @@ import csv
 import sys
 import profile
 
-def readCSV01():
-	#from documentation
-	d={}
-	with open(sys.argv[1], 'rt') as f:
-		reader = csv.DictReader(f)
-		for row in reader:
-			print row
-
-print 'RAW'
-profile.run('print readCSV01(); print')
 
 
 
