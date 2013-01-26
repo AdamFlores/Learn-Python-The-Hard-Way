f92a1ee487ec8351088d1a7ca59d887ff4af2744
Adam Flores <rendenmedia@gmail.com>
2013-01-25 22:26:44 -0500

    Processing Threads and Event Handlers
    Testing to make a clear split and integration with Actors, Views, and
    Logic early.

---
+++ engine/my_demo_multiple_scenes_01_unit_test_02.py
@@ -0,0 +1,35 @@
+#
+# cocos2d
+# http://cocos2d.org
+#
+# This code is so you can run the samples without installing the package
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+#
+
+
+import cocos
+from cocos.director import director
+from cocos.actions import *
+from cocos.layer import *
+import pyglet
+
+class TestLayer(cocos.layer.Layer):
+    def __init__(self):
+        super( TestLayer, self ).__init__()
+        x,y = director.get_window_size()
+        
+if __name__ == "__main__":
+    director.init( resizable=False )
+    #main_scene = cocos.scene.Scene()
+    main_scene = cocos.scene.Scene() #Bulk of "processing" not "events" will handle in Scene
+    #main_scene.transform_anchor = (320,240)
+    #child1_scene = cocos.scene.Scene()
+    #child1_scene.add( ColorLayer( 0,0,255,255 ) )
+    #child1_scene.scale = 1.5
+    #child1_scene.position = (-160,-120)
+    #child1_scene.transform_anchor = (320,240)
+    #main_scene.add( child1_scene )
+    #director.run (main_scene)
+    director.run (main_scene)
