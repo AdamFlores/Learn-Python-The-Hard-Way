0b705d003de63da472c483d2342655321b7e590b
Adam Flores <rendenmedia@gmail.com>
2013-02-08 19:44:56 -0500

    More Tests of List Functions and Methods
    No asserts, but still small unit tests. The print statements are helping
    me to figure the number or letters that will go into the asserts.

--- LearnPython273Aptana/ListUnitTest02.py
+++ LearnPython273Aptana/ListUnitTest02.py
@@ -7,6 +7,21 @@ class ListUnitTest02(unittest.TestCase):
 
 	def test_print(self):
 		print self.list_unit_test_01
+		
+	def test_list_for(self):
+		for letter in self.list_unit_test_01:
+			print letter
+			
+	def test_list_if(self):
+		if "A" in self.list_unit_test_01:
+			print "A is in List"
+			
+	def test_list_slice(self):
+		begin = int(2)
+		end = int(3)
+		print "Slice:"
+		print self.list_unit_test_01[begin:end]
+		
 	
 	def test_list_assert(self):
 		self.assertEqual(self.list_unit_test_01, ["A","B","C","D"])
