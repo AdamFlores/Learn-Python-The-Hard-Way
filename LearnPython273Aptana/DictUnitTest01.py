1596793eb5dcd78e86e619a74f0d2dc641cd7e8c
Adam Flores <rendenmedia@gmail.com>
2013-02-09 16:42:54 -0500

    First Test Compile
    Test Results are almost illegible, but this feels right and is exciting.

--- LearnPython273Aptana/DictUnitTest01.py
+++ LearnPython273Aptana/DictUnitTest01.py
@@ -1,33 +1,33 @@
 import unittest
 
-class DictUnitTest02(unittest.TestCase):
+class DictUnitTest01(unittest.TestCase):
     
     def setUp(self):
-        self.dict_unit_test_01 = ["A":"Adam", "B":"Barry" , "C":"Cherry", "D":"Donkey"]
+        self.dict_unit_test_01 = {"A":"Adam", "B":"Barry" , "C":"Cherry", "D":"Donkey"}
 
     def test_print(self):
         print self.dict_unit_test_01
         
-    def test_list_for(self):
+    def test_dict_for(self):
         for letter in self.dict_unit_test_01:
             print letter
             
-    def test_list_if(self):
+    def test_dict_if(self):
         if "A" in self.dict_unit_test_01:
             print "A is in List"
             
-    def test_list_slice(self):
+    def test_dict_slice(self):
         begin = int(2)
         end = int(3)
         print "Slice:"
-        print self.dict_unit_test_01[begin:end]
+        print self.dict_unit_test_01{begin:end}
         
     
-    def test_list_assert(self):
+    def test_dict_assert(self):
         print "This Test Should Pass:"
-        self.assertEqual(self.dict_unit_test_01, ["A":"Adam", "B":"Barry" , "C":"Cherry", "D":"Donkey"])
+        self.assertEqual(self.dict_unit_test_01, {"A":"Adam", "B":"Barry" , "C":"Cherry", "D":"Donkey"})
         print "This Test Should Fail:"
-        self.assertNotEqual(self.dict_unit_test_01, ["A":"Adam", "B":"Barry" , "C":"Cherry", "D":"Donkey"])
+        self.assertNotEqual(self.dict_unit_test_01, {"A":"Adam", "B":"Barry" , "C":"Cherry", "D":"Donkey"})
 
 if __name__ == "__main__":
     unittest.main()
