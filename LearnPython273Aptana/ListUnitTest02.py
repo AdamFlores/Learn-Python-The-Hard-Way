b22e7913554e651d4d8ca947d0590351bdfb72d3
Adam Flores <rendenmedia@gmail.com>
2013-02-08 19:04:35 -0500

    Successful Assert and Unittest
    Starting to really get it.

--- LearnPython273Aptana/ListUnitTest02.py
+++ LearnPython273Aptana/ListUnitTest02.py
@@ -7,6 +7,10 @@ class ListUnitTest02(unittest.TestCase):
 
 	def test_print(self):
 		print self.list_unit_test_01
+	
+	def test_list_assert(self):
+		self.assertEqual(self.list_unit_test_01, ["A","B","C","D"])
+		self.assertNotEqual(self.list_unit_test_01, ["A","B","C","C"])
 
 if __name__ == "__main__":
 	unittest.main()
