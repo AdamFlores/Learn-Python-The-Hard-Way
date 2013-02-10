fcc9efa02d3cad000b4ae8fab31246cccac46de2
Adam Flores <rendenmedia@gmail.com>
2013-02-09 21:52:54 -0500

    Director Unit Test continued

---
+++ LearnPython273Aptana/Cocos2dDirectorUnitTest02.py
@@ -0,0 +1,17 @@
+import cocos
+import unittest
+
+
+
+class DirectorUnitTest02(unittest.TestCase):
+    def setup(self):
+            class HelloWorld(cocos.layer.Layer):
+                def __init__(self):
+                    super( HelloWorld, self ).__init__()
+    def test_director_init(self):
+                if __name__ == "__main__":
+                    # director init takes the same arguments as pyglet.window
+                    cocos.director.director.init()
+
+if __name__ == "__main__":
+    unittest.main()
